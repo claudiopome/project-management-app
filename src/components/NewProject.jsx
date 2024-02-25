dedb1be9968f60f53b6781eb9b999eab307a4035
claudiopome <claudiopome@gmail.com>
2024-02-25 16:35:02 +0100

    add button styling.

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -10,7 +10,7 @@ export default function NewProject() {
           </button>
         </li>
         <li>
-          <button className="bg-stone-800 text-stone-50 hover:bg-stone-950">
+          <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">
             Save
           </button>
         </li>
