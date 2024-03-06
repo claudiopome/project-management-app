6021a91b81962e71f1745d5c9af8751429f7d1f4
claudiopome <claudiopome@gmail.com>
2024-03-06 10:56:16 +0100

    wip: handle delete project.

--- src/App.jsx
+++ src/App.jsx
@@ -53,6 +53,18 @@ function App() {
     });
   }
 
+  function handleDeleteProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter(
+          (project) => project.id !== prevState.selectedProjectId
+        ),
+      };
+    });
+  }
+
   const selectedProject = projectsState.projects.find(
     (project) => project.id === projectsState.selectedProjectId
   );
